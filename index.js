5539159eb0daeb32f8371ae0875ca40e868f9156
caipinfang <caipinfang@meituan.com>
2019-09-06 16:28:39 +0800

    feat:完成

--- index.js
+++ index.js
@@ -14,10 +14,19 @@ function h(type, props, ...children) {
   return { type, props, children: flatten(children) };
 }
 function view(count) {
+  // return (
+  //   <ul id="cool" className="foo">
+  //     <li className="test">test1</li>
+  //     <li>test2</li>
+  //   </ul>
+  // );
+  const r = [...Array(count).keys()];
+  console.log("查看r", r);
   return (
-    <ul id="cool" className="foo">
-      <li className="test">test1</li>
-      <li>test2</li>
+    <ul id="cool" className={`my-class-${count % 3}`}>
+      {r.map(n => (
+        <li>item{(count * n).toString()}</li>
+      ))}
     </ul>
   );
 }
@@ -26,9 +35,126 @@ function createElement(node) {
     return document.createTextNode(node);
   }
   const el = document.createElement(node.type);
+  setProps(el, node.props);
   node.children.map(createElement).forEach(el.appendChild.bind(el));
   return el;
 }
+function setProp(target, name, value) {
+  if (name === "className") {
+    return target.setAttribute("class", value);
+  }
+  target.setAttribute(name, value);
+}
+function setProps(target, props) {
+  Object.keys(props).forEach(name => {
+    setProp(target, name, props[name]);
+  });
+}
+function changed(node1, node2) {
+  return (
+    typeof node1 !== typeof node2 ||
+    (typeof node1 === "string" && node1 !== node2) ||
+    node1.type !== node2.type
+  );
+}
+function diffProps(newNode, oldNode) {
+  const patches = [];
+  const props = Object.assign({}, newNode.props, oldNode.props);
+  Object.keys(props).forEach(name => {
+    const newVal = newNode.props[name];
+    const oldVal = oldNode.props[name];
+    if (!newVal) {
+      patches.push({ type: REMOVE_PROP, name, value: oldVal });
+    } else {
+      patches.push({ type: SET_PROP, name, value: newVal });
+    }
+  });
+  return patches;
+}
+function diff(newNode, oldNode) {
+  if (!oldNode) {
+    return { type: CREATE, newNode };
+  }
+  if (!newNode) {
+    return { type: REMOVE };
+  }
+  if (changed(newNode, oldNode)) {
+    return { type: REPLACE, newNode };
+  }
+  if (newNode.type) {
+    return {
+      type: UPDATE,
+      children: diffChildren(newNode, oldNode),
+      props: diffProps(newNode, oldNode)
+    };
+  }
+}
+function diffChildren(newNode, oldNode) {
+  const patches = [];
+  const patchesLength = Math.max(
+    newNode.children.length,
+    oldNode.children.length
+  );
+  for (let i = 0; i < patchesLength; i++) {
+    patches[i] = diff(newNode.children[i], oldNode.children[i]);
+  }
+  return patches;
+}
+function removeProp(target, name, value) {
+  if (name === "className") {
+    return target.removeAttribute("class");
+  }
+  target.removeAttribute(name);
+}
+function patchProps(parent, patches) {
+  for (let i = 0; i < patches.length; i++) {
+    const propPatch = patches[i];
+    const { type, name, value } = propPatch;
+    if (type === SET_PROP) {
+      setProp(parent, name, value);
+    }
+    if (type === REMOVE_PROP) {
+      removeProp(parent, name, value);
+    }
+  }
+}
+function patch(parent, patches, index = 0) {
+  if (!patches) {
+    return;
+  }
+  const el = parent.childNodes[index];
+  switch (patches.type) {
+    case CREATE: {
+      const { newNode } = patches;
+      const newEl = createElement(newNode);
+      return parent.appendChild(newEl);
+    }
+    case REMOVE: {
+      return parent.removeChild(el);
+    }
+    case REPLACE: {
+      const { newNode } = patches;
+      const newEl = createElement(newNode);
+      return parent.replaceChild(newEl, el);
+    }
+    case UPDATE: {
+      const { children, props } = patches;
+      patchProps(el, props);
+      for (let i = 0; i < children.length; i++) {
+        patch(el, children[i], i);
+      }
+    }
+  }
+}
+
+function tick(el, count) {
+  const patches = diff(view(count + 1), view(count));
+  patch(el, patches);
+  console.log(count, patches);
+  if (count > 20) return;
+  setTimeout(() => tick(el, count + 1), 500);
+}
 function render(el) {
-  el.appendChild(createElement(view(0)));
+  el.appendChild(createElement(view(1)));
+  setTimeout(() => tick(el, 0), 500);
 }
